8fffc4f212078f205abc63feb68b230e444852a6
Paul Soderquist <paulsoderquist3@gmail.com>
2015-12-02 23:06:51 -0700

    added enter device id into add child and changed it to be placeholder instead of labeled

--- js/devices.js
+++ js/devices.js
@@ -6,7 +6,7 @@ var curr = $('.page').html();
 
     	$(this).siblings('.add-child').remove();
 
-    	var adding = "<div class='add-child'><div class='name-title'>Name: </div><input id='new-name' type='text' name='name'><div class='save-d'><div class='save-text'>Save</div><div class='cancel-text'>cancel</div></div><div>";
+    	var adding = "<div class='add-child'><div class='input-field'><input id='new-name' style='width:100%' type='text' name='name' placeholder='name'></div><div class='input-field'><input id='new-name' style='width:100%' type='text' name='id' placeholder='device id'></div><div class='save-d'><div class='save-text'>Save</div><div class='cancel-text'>cancel</div></div><div>";
     	$('.page').append(adding);
 
     	$('.save-text').on('click',function(){
